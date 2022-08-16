45740e3e9dae1462cac9e3023fbd52c571cd7e08
ypfland <yukofland@gmail.com>
2022-08-16 15:03:40 +0800

    ubah title branch yuko

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'app/routes/app_pages.dart';
 void main() {
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "branch yuko",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
